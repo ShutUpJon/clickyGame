8c4c609df8484ce7f5abd952297700e7181a30ed
jonlawrence83@gmail.com <jonlawrence83@gmail.com>
2019-01-18 12:55:40 -0700

    final

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Nav.css";
+
+const Nav = props => (
+     <nav>
+          <ul>
+               <li className="brand animated lightSpeedIn">
+                    <a href="clickyGame">{props.title}</a>
+               </li>
+               <li id="rw">{props.rightWrong}</li>
+               <li id="cur-sco">Current Score: {props.score}</li>
+               <li id="top-sco">Top Score: {props.topScore}</li>
+          </ul>
+     </nav>
+);
+
+export default Nav;
